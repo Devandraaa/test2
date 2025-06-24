8b45ec292cdd384ecce3fcaaa3882637fb67c45b
Devandraaa <raisasatyaputra@gmail.com>
2025-06-24 16:30:26 +0700

    Add files via upload

---
+++ portofolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {More actions
+    strings: ["Computer Science Student", "Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
